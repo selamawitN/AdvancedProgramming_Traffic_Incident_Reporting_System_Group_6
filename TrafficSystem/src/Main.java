d73f103c962553c735f43212eccd5a2441de1aee
selamawitN <selamawit.namssi@gmail.com>
2026-06-02 12:51:38 +0300

    Initialize Traffic System with server and GUI setup

--- TrafficSystem/src/Main.java
+++ TrafficSystem/src/Main.java
@@ -1,1 +1,20 @@
+import database.DatabaseConnection;
+import gui.LoginScreen;
+import gui.SplashScreen;
+import network.TrafficServer;
+import network.WebServer;
+import javax.swing.SwingUtilities;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        
+        new Thread(() -> TrafficServer.main(null)).start();
+        Thread.sleep(2000);
+  
+        SwingUtilities.invokeLater(() -> new SplashScreen());
+        Thread.sleep(3000);
+      
+        DatabaseConnection.createTables();
+        SwingUtilities.invokeLater(() -> new LoginScreen());
+    }
+}
