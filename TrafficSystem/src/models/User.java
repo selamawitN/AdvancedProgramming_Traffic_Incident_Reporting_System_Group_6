0210e28af1162351108f2439764d4175d6b2216f
selamawitN <selamawit.namssi@gmail.com>
2026-06-02 13:07:15 +0300

    Add User class with properties and methods

--- TrafficSystem/src/models/User.java
+++ TrafficSystem/src/models/User.java
@@ -1,1 +1,34 @@
+package models;
 
+public class User {
+
+    private int id;        
+    private String name;      
+    private String email;    
+    private String password;  
+    private String role;      
+
+    public User(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId()           { return id; }
+    public String getName()      { return name; }
+    public String getEmail()     { return email; }
+    public String getPassword()  { return password; }
+    public String getRole()      { return role; }
+
+    public void setId(int id)              { this.id = id; }
+    public void setName(String name)       { this.name = name; }
+    public void setEmail(String email)     { this.email = email; }
+    public void setPassword(String p)      { this.password = p; }
+    public void setRole(String role)       { this.role = role; }
+
+    @Override
+    public String toString() {
+        return "User[id=" + id + ", name=" + name + ", role=" + role + "]";
+    }
+}
